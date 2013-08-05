7714aeca71f06864e457457e89965faa31065e97
James Douglass <jdouglass@stanford.edu>
2013-08-05 14:44:40 -0700

    Update issue 2207
    Updating the structure of the __init__ file a little to make more sense
    when we add attributes later on.

--- adept/__init__.py
+++ adept/__init__.py
@@ -1,9 +1,11 @@
 import versioning
 
 # The __version__ attribute MUST be set to 'dev'.  It is changed automatically
-# when the package is built.  The build_data attribute is set at the same time.
+# when the package is built.  The build_attrs attribute is set at the same time,
+# but it instead contains a list of attributes of __init__.py that are related
+# to the build.
 __version__ = 'dev'
-build_data = None
+build_attrs= None
 
 if __version__ == 'dev' and build_data == None:
     __version__ = versioning.version()
