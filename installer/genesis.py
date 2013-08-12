caccfb7cedb98b8f812df525e34f387ea719437c
James Douglass <jdouglass@stanford.edu>
2013-08-12 10:27:36 -0700

    Update issue 2210
    Refactoring some data out of functions because they don't actually do
    anyhting except return a value.  Makes more sense (at the moment) to
    have them as static data.

--- installer/genesis.py
+++ installer/genesis.py
@@ -3,34 +3,50 @@
 import demjson  # using for js compaibility -- comments in JSON files.
 import sys
 
-def build_installer_script(config_file_uri, out_file_uri):
-    new_file = open(out_file_uri, 'w')
-    config_dict = demjson.decode(open(config_file_uri).read())
+LOG_FILE_SCRIPT = """
+Fuinction DumpLog
+    Exch $5
+    Push $0
+    Push $1
+    Push $2
+    Push $3
+    Push $4
+    Push $6
 
-    # check for default values before writing the script.
-    sanitized_config = check_defaults(config_dict)
+    FindWindow $0 "#32770" "" $HWNDPARENT
+    GetDlgItem $0 $0 1016
+    StrCmp $0 0 exit
+    FileOpen $5 $5 "w"
+    StrCmp $5 "" exit
+        SendMessage $0 ${LVM_GETITEMCOUNT} 0 0 $6
+        System::Alloc ${NSIS_MAX_STRLEN}
+        Pop $3
+        StrCpy $2 0
+        System::Call "*(i, i, i, i, i, i, i, i, i) i \\
+            (0, 0, 0, 0, 0, r3, ${NSIS_MAX_STRLEN}) .r1"
+        loop: StrCmp $2 $6 done
+            System::Call "User32::SendMessageA(i, i, i, i) i \\
+            ($0, ${LVM_GETITEMTEXT}, $2, r1)"
+            System::Call "*$3(&t${NSIS_MAX_STRLEN} .r4)"
+            FileWrite $5 "$4$\\r$\\n"
+            IntOp $2 $2 + 1
+            Goto loop
+        done:
+            FileClose $5
+            System::Free $1
+            System::Free $3
+    exit:
+        Pop $6
+        Pop $4
+        Pop $3
+        Pop $2
+        Pop $1
+        Pop $0
+        Exch $5
+FunctionEnd
+    """
 
-    new_file.write(local_variables(sanitized_config))
-    new_file.write(general_settings())
-
-    if sanitized_config['installer']['save_log']:
-        new_file.write(save_log_file_function())
-
-    #TODO: write the .onInit function
-
-    new_file.write(installer(sanitized_config['installer']))
-    new_file.write(uninstaller())
-
-    new_file.close()
-
-def check_defaults(config_dictionary):
-    # Assume that everything needed is there for now.
-    # TODO: actually sanitize the config dictionary.  Raise needed exceptions,
-    # or else assume certain defaults.
-    return config_dictionary
-
-def general_settings():
-    general_settings = """
+GENERAL_SETTINGS = """
 ; Set the compression size and type.
 SetCompressor /FINAL /SOLID lzma
 SetCompressorDictSize 64
@@ -46,8 +62,67 @@ OutFile "${INSTALLER_FILENAME}"
 InstallDir "${DEFAULT_INSTALL_DIR}"
 ShowInstDetails show
 """
-    return general_settings
 
+SAVE_LOG_FILE = """
+    ; Write the install log to a text file on disk.
+    StrCpy $0 "$INSTDIR\\${INSTALLER_LOGFILE}"
+    Push $0
+    Call DumpLog
+    """
+
+UNINSTALLER_SECTION = """
+Section "uninstall"
+    ; Need to enforce execution level as admin.  See
+    ; nsis.sourceforge.net/Shortcuts_removal_fails_on_Windows_Vista
+    SetShellVarContext all
+    rmdir /r "$SMPROGRAMS\${START_MENU_FOLDER}"
+
+    ; Delete the installation directory on disk
+    rmdir /r "$INSTDIR"
+
+    ; Delete the entire registry key for this version of RIOS.
+    DeleteRegKey HKLM "${REGISTRY_PATH}"
+SectionEnd
+"""
+
+UNINSTALLER_REG_KEYS = """
+    WriteRegStr HKLM "${REGISTRY_PATH}" "DisplayName"          "${APP_NAME} ${APP_VERSION}"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "UninstallString"      "$INSTDIR\\${UNINSTALLER_FILENAME}.exe"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "QuietUninstallString" "$INSTDIR\\${UNINSTALLER_FILENAME}.exe /S"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "InstallLocation"      "$INSTDIR"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "DisplayIcon"          "$INSTDIR\\${ICON}"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "Publisher"            "${PUBLISHER}"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "URLInfoAbout"         "${WEBSITE}"
+    WriteRegStr HKLM "${REGISTRY_PATH}" "DisplayVersion"       "${APP_VERSION}"
+    WriteRegDWORD HKLM "${REGISTRY_PATH}" "NoModify" 1
+    WriteRegDWORD HKLM "${REGISTRY_PATH}" "NoRepair" 1
+    """
+
+def build_installer_script(config_file_uri, out_file_uri):
+    new_file = open(out_file_uri, 'w')
+    config_dict = demjson.decode(open(config_file_uri).read())
+
+    # check for default values before writing the script.
+    sanitized_config = check_defaults(config_dict)
+
+    new_file.write(local_variables(sanitized_config))
+    new_file.write(GENERAL_SETTINGS)
+
+    if sanitized_config['installer']['save_log']:
+        new_file.write(LOG_FILE_SCRIPT)
+
+    #TODO: write the .onInit function
+
+    new_file.write(installer(sanitized_config['installer']))
+    new_file.write(UNINSTALLER_SECTION)
+
+    new_file.close()
+
+def check_defaults(config_dictionary):
+    # Assume that everything needed is there for now.
+    # TODO: actually sanitize the config dictionary.  Raise needed exceptions,
+    # or else assume certain defaults.
+    return config_dictionary
 
 def installer_pages():
     pages_string = """
@@ -120,74 +195,6 @@ def start_menu_links(options):
 
     return formatted_string
 
-def uninstaller_registry_keys():
-    formatted_string = """
-    WriteRegStr HKLM "${REGISTRY_PATH}" "DisplayName"          "${APP_NAME} ${APP_VERSION}"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "UninstallString"      "$INSTDIR\\${UNINSTALLER_FILENAME}.exe"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "QuietUninstallString" "$INSTDIR\\${UNINSTALLER_FILENAME}.exe /S"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "InstallLocation"      "$INSTDIR"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "DisplayIcon"          "$INSTDIR\\${ICON}"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "Publisher"            "${PUBLISHER}"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "URLInfoAbout"         "${WEBSITE}"
-    WriteRegStr HKLM "${REGISTRY_PATH}" "DisplayVersion"       "${APP_VERSION}"
-    WriteRegDWORD HKLM "${REGISTRY_PATH}" "NoModify" 1
-    WriteRegDWORD HKLM "${REGISTRY_PATH}" "NoRepair" 1
-    """
-
-    return formatted_string
-
-def save_log_file():
-    return """
-    ; Write the install log to a text file on disk.
-    StrCpy $0 "$INSTDIR\\${INSTALLER_LOGFILE}"
-    Push $0
-    Call DumpLog
-    """
-
-def save_log_file_function():
-    return """
-Fuinction DumpLog
-    Exch $5
-    Push $0
-    Push $1
-    Push $2
-    Push $3
-    Push $4
-    Push $6
-
-    FindWindow $0 "#32770" "" $HWNDPARENT
-    GetDlgItem $0 $0 1016
-    StrCmp $0 0 exit
-    FileOpen $5 $5 "w"
-    StrCmp $5 "" exit
-        SendMessage $0 ${LVM_GETITEMCOUNT} 0 0 $6
-        System::Alloc ${NSIS_MAX_STRLEN}
-        Pop $3
-        StrCpy $2 0
-        System::Call "*(i, i, i, i, i, i, i, i, i) i \\
-            (0, 0, 0, 0, 0, r3, ${NSIS_MAX_STRLEN}) .r1"
-        loop: StrCmp $2 $6 done
-            System::Call "User32::SendMessageA(i, i, i, i) i \\
-            ($0, ${LVM_GETITEMTEXT}, $2, r1)"
-            System::Call "*$3(&t${NSIS_MAX_STRLEN} .r4)"
-            FileWrite $5 "$4$\\r$\\n"
-            IntOp $2 $2 + 1
-            Goto loop
-        done:
-            FileClose $5
-            System::Free $1
-            System::Free $3
-    exit:
-        Pop $6
-        Pop $4
-        Pop $3
-        Pop $2
-        Pop $1
-        Pop $0
-        Exch $5
-FunctionEnd
-    """
-
 def installer(installer_options):
     formatted_string = """
 Section \"Install\" SEC01\n
@@ -200,31 +207,14 @@ Section \"Install\" SEC01\n
     """
 
     formatted_string += start_menu_links(installer_options['start_menu'])
-    formatted_string += uninstaller_registry_keys()
+    formatted_string += UNINSTALLER_REG_KEYS
 
     if installer_options['save_log']:
-        formatted_string += save_log_file()
+        formatted_string += SAVE_LOG_FILE
 
     formatted_string += """
 SectionEnd
 """
 
-    return formatted_string
-
-def uninstaller():
-    formatted_string = """
-Section "uninstall"
-    ; Need to enforce execution level as admin.  See
-    ; nsis.sourceforge.net/Shortcuts_removal_fails_on_Windows_Vista
-    SetShellVarContext all
-    rmdir /r "$SMPROGRAMS\${START_MENU_FOLDER}"
-
-    ; Delete the installation directory on disk
-    rmdir /r "$INSTDIR"
-
-    ; Delete the entire registry key for this version of RIOS.
-    DeleteRegKey HKLM "${REGISTRY_PATH}"
-SectionEnd
-"""
     return formatted_string
 
