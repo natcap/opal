73ebb2dc4535a7d1db85efd0f43b2c749d505bae
James Douglass <jdouglass@stanford.edu>
2013-08-13 13:35:53 -0700

    Update issue 2210
    Adding a build batch script for jenkins/Quincy

---
+++ jenkins_build.bat
@@ -0,0 +1,14 @@
+SET ENVDIR=adept_environment
+DEL /S /Q build
+DEL /S /Q %ENVDIR%
+
+SET PYTHON=C:\py27_build\python.exe
+
+%PYTHON% bootstrap_adept_environment.py > setup_environment.py
+%PYTHON% setup_environment.py --clear --system-site-packages %ENVDIR%
+copy C:\Python27\Lib\distutils\distutils.cfg .\%ENVDIR%\Lib\distutils\distutils.cfg
+cd invest-natcap.invest-3
+..\%ENVDIR%\Scripts\python setup.py install
+cd ..
+
+%ENVDIR%\Scripts\python setup.py py2exe win_installer
