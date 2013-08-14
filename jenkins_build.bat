a965bffff129393145e5fd7fd1a4d8a5f07b3e52
James Douglass <jdouglass@stanford.edu>
2013-08-14 09:09:06 -0700

    Update issue 2210
    Adding a call to explicitly build the invest cython extensions.

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -9,7 +9,7 @@ IF NOT "%1" == "" SET PYTHON="%1"
 %PYTHON% setup_environment.py --clear --system-site-packages %ENVDIR%
 copy C:\Python27\Lib\distutils\distutils.cfg .\%ENVDIR%\Lib\distutils\distutils.cfg
 cd invest-natcap.invest-3
-..\%ENVDIR%\Scripts\python setup.py install
+..\%ENVDIR%\Scripts\python setup.py build_ext install
 cd ..
 
 %ENVDIR%\Scripts\python setup.py py2exe win_installer bdist_wininst
