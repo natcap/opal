ede558dc2ea66a5327410dd4e2011b93544211ea
James Douglass <jdouglass@stanford.edu>
2013-08-19 15:19:04 -0700

    Update issue 2258
    Adding an initial execute function for adept, located in adept_core.

---
+++ adept/adept_core.py
@@ -0,0 +1,26 @@
+"""Adept_core contains the core logic for the adept permitting tool."""
+
+import logging
+
+import invest_natcap
+
+logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
+     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('adept')
+
+def execute(args):
+    """The main execution function for adept.
+        args - a python dictionary containing the following elements:
+            <none>
+
+        Returns nothing."""
+
+    LOGGER.debug('   _____       .___             __   ')
+    LOGGER.debug('  /  _  \\    __| _/____ _______/  |_ ')
+    LOGGER.debug(' /  /_\\  \\  / __ |/ __ \\\\____ \\   __\\\\')
+    LOGGER.debug('/    |    \\/ /_/ \\  ___/|  |_> >  |  ')
+    LOGGER.debug('\\____|__  /\\____ |\\___  >   __/|__|  ')
+    LOGGER.debug('        \\/      \\/    \\/|__|         ')
+    LOGGER.debug('                                     ')
+
