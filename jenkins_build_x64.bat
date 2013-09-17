4182a4986d5d825ed3bd4bfe0d1d25f74332b28c
James Douglass <jdouglass@stanford.edu>
2013-09-17 15:28:21 -0700

    Adding debigging printing to the x64 jenkins build

--- jenkins_build_x64.bat
+++ jenkins_build_x64.bat
@@ -9,6 +9,7 @@ IF NOT "%1" == "" SET PYTHON="%1"
 %PYTHON% setup_environment.py --clear --system-site-packages %ENVDIR%
 
 cd invest-natcap.invest-3
+echo %~dp0
 echo "--- Compiling invest-3 ---"
 ..\%ENVDIR%\Scripts\python setup.py build_ext install
 cd ..
@@ -16,6 +17,7 @@ cd ..
 echo "--- CD to Adept folder ---"
 
 echo "--- Building Adept ---"
+echo %~dp0
 SET PATH=%PATH%;%ENVDIR%\Lib\site-packages
 %ENVDIR%\Scripts\python setup.py py2exe win_installer bdist_wininst
 echo "--- Finished building Adept ---"
