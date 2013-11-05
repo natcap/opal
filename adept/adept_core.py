092f4b48b519267684b9b9c345a558d6f9ff6195
richpsharp <richpsharp@gmail.com>
2013-11-05 12:15:36 -0800

    Update issue 2440
    Handling the case where the webpage directory might exist because of a previous run.  Rather than crash, just dump a warning and keep going.

--- adept/adept_core.py
+++ adept/adept_core.py
@@ -17,8 +17,7 @@ def execute(args):
         args - a python dictionary containing the following elements:
            'project_footprint_uri' - a URI to a shapefile of the project
                 footprint
-            'impact_type' - 0 or 1 depending on if it's a road/mine or
-                bare ground/paved
+            'impact_type' - "Road/Mine" or "Bare ground/Paved".
             'area_of_influence_uri' - a uri to a shapefile that defines the
                 area of influcence of the impact site
             'custom_static_map_uri' - a uri to a custom static map to assess
@@ -50,7 +49,7 @@ def execute(args):
     impact_dict = {
         0: {
             'Site':os.path.basename(args['project_footprint_uri']),
-            'Impact Type': 'null',
+            'Impact Type': args['impact_type'],
             'Impact Amount': custom_static_values_flat.total.values()[0]
         },
     }
@@ -65,11 +64,18 @@ def execute(args):
         os.path.join(args['workspace_dir'], report_data_source_directory))
     
     if os.path.exists(report_data_out_directory):
-        shutil.rmtree(report_data_out_directory)
+        try:
+            shutil.rmtree(report_data_out_directory)
+        except OSError as e:
+            LOGGER.warn(e)
     
-    shutil.copytree(
-        report_data_source_directory,
-        report_data_out_directory)
+    try:
+        shutil.copytree(
+            report_data_source_directory,
+            report_data_out_directory)
+    except OSError as e:
+            LOGGER.warn(e)
+    
     
     css_uri = os.path.join(
         report_data_source_directory, 'table_style.css')
