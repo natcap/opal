5c948cc2495aeed0f88bf7d821f7c3ed74ca5d46
richpsharp <richpsharp@gmail.com>
2013-11-20 10:44:19 -0800

    Update issue 2240
    Fixed a degenerate polygon which made the intersection test much faster.

--- intersection_tracer.py
+++ intersection_tracer.py
@@ -7,6 +7,8 @@ import shapely.geometry
 import shapely.wkb
 import shapely.speedups
 import shapely.prepared
+import shapely.ops
+import shutil
 
 logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -16,6 +18,12 @@ LOGGER = logging.getLogger('intersection_tracer')
 impact_ds_uri = './data/colombia_tool_data/Example permitting footprints/Example_mining_projects.shp'
 ecosystems_ds_uri = ('./data/colombia_tool_data/ecosys_dis_nat_comp_fac.shp')
 clipped_uri = 'impact_areas'
+
+try:
+    shutil.rmtree(clipped_uri)
+except OSError as e:
+    print 'warning: ', e
+
 
 ecosystems_ds = ogr.Open(ecosystems_ds_uri)
 print ecosystems_ds
@@ -31,6 +39,7 @@ clipped_layer = clipped_ds.CreateLayer(
 print ecosystems_ds_defn
 
 #for field_index in xrange(ecosystems_ds_defn.GetFieldCount()):
+relevant_field_names = ['FACTOR_DE', 'Ecos_dis']
 for field_name in ['FACTOR_DE', 'Ecos_dis']:
     field_index = ecosystems_ds_defn.GetFieldIndex(field_name)
     print field_index, field_name
@@ -40,7 +49,6 @@ for field_name in ['FACTOR_DE', 'Ecos_dis']:
     current_def.SetPrecision(current_field.GetPrecision())
     clipped_layer.CreateField(current_def)
 
-
 #1) Load the impact dataset into a shapely multipolygon
 impact_ds = ogr.Open(impact_ds_uri)
 impact_layer = impact_ds.GetLayer()
@@ -49,33 +57,32 @@ for feature_index in xrange(impact_layer.GetFeatureCount()):
     feature = impact_layer.GetFeature(feature_index)
     geometry = feature.GetGeometryRef()
     polygon_list.append(shapely.wkb.loads(geometry.ExportToWkb()))
-multipolygon = shapely.geometry.MultiPolygon(polygon_list)
+multipolygon = shapely.ops.cascaded_union(polygon_list)
+
+if not multipolygon.is_valid:
+    print 'permitting area is not valid'
+    sys.exit(1)
 
 #2) Make a spatial index?
-
-print 'speedups available? ', shapely.speedups.available
-shapely.speedups.enable()
+if shapely.speedups.available:
+    print 'speedups available. speeding up'
+    shapely.speedups.enable()
 
 #3) Loop through each feature in impact ds and build a polygon out of it
 for feature_index in xrange(ecosystems_ds_layer.GetFeatureCount()):
-    print 'intersecting permitting layer with ecosystem ', feature_index
+    print '.', 
     feature = ecosystems_ds_layer.GetFeature(feature_index)
+    ecosystem_type = feature.GetField('Ecos_dis')
+    impact_factor = feature.GetField('FACTOR_DE')
     geometry = feature.GetGeometryRef()
-    print 'building polygon'
     polygon = shapely.wkb.loads(geometry.ExportToWkb())
     prepared_polygon = shapely.prepared.prep(polygon)
-    print 'intersecting polygon'
     if prepared_polygon.intersects(multipolygon):
-        print 'intersects'
-
-        print 'simplifying'
-        #simple = polygon.simplify(0.9, False)
+        print 'permitting area intersects ', ecosystem_type, 'calculating area overlap'
         intersection = multipolygon.intersection(polygon)
-        print intersection.area
-        sys.exit(1)
-    else:
-        print 'not contained'
-    
+        print 'overlaps ', intersection.area / 10000.0, 'Ha required offset: ', 
+        print intersection.area / 10000.0 * impact_factor, ' Ha'
+        
 
 
 #3a) intersect that polygon with the impact multipolygon
