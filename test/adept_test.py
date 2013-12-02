d4034e81540a640d20ddb0dcd69d7f52cca800fd
James Douglass <jdouglass@stanford.edu>
2013-12-02 12:59:52 -0800

    Update issue 2498
    Removing adept-related files that have been moved over to
    invest-natcap.adept.

--- test/adept_test.py
+++
@@ -1,8 +0,0 @@
-import adept
-
-import invest_natcap.testing
-
-class AdeptTest(invest_natcap.testing.GISTest):
-    def test_smoke(self):
-        """A simple smoke test for adept."""
-        adept.execute({})
