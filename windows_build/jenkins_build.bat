a30c4dee4ff2043d1642543da5f4242a2452fa6e
James Douglass <jdouglass@stanford.edu>
2013-12-03 10:51:49 -0800

    Update issue 2498
    Adding a couple of lines to the jenkins build batch script to install
    adept to the virtualenv.

--- windows_build/jenkins_build.bat
+++ windows_build/jenkins_build.bat
@@ -8,8 +8,17 @@ IF NOT "%1" == "" SET PYTHON="%1"
 %PYTHON% bootstrap_adept_environment.py > setup_environment.py
 %PYTHON% setup_environment.py --clear --system-site-packages %ENVDIR%
 copy C:\Python27\Lib\distutils\distutils.cfg .\%ENVDIR%\Lib\distutils\distutils.cfg
+
+:: CD to the invest-3 directory to install it to the virtual environment
 cd invest-natcap.invest-3
 ..\%ENVDIR%\Scripts\python setup.py build_ext install
 cd ..
 
+:: CD to the Adept directory to install adept to the virtual environment
+cd adept
+..\%ENVDIR%\Scripts\python setup.py install
+cd ..
+
+:: Now that everything is installed, we can run the permitting project's
+:: setup.py commands to build everything we want/need.
 %ENVDIR%\Scripts\python setup.py py2exe win_installer bdist_wininst
