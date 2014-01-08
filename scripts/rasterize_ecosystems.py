9ee767577030b11d5ebc494052295834cd552c3d
James Douglass <jdouglass@stanford.edu>
2014-01-07 16:03:36 -0800

    Update issue 2558
    Adding a little python script to rasterize the clipped ecosystems
    vector.

---
+++ scripts/rasterize_ecosystems.py
@@ -0,0 +1,11 @@
+import gdal
+from invest_natcap import raster_utils
+
+lulc_vector = 'data/colombia_clipped/ecosystems_colombia.shp'
+target_raster = 'data/colombia_clipped/ecosystems.tif'
+sample_raster = 'data/colombia_clipped/dem.tif'
+
+raster_utils.new_raster_from_base_uri(sample_raster, target_raster, 'GTiff', -1,
+        gdal.GDT_Byte)
+raster_utils.rasterize_layer_uri(target_raster, lulc_vector,
+        option_list=["ATTRIBUTE=lucode"])
