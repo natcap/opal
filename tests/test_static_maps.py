0b64d8922d9d78f713e2b8220df79c7e93c7f9a3
James Douglass <jdouglass@stanford.edu>
2014-01-07 16:02:58 -0800

    Update issue 2558
    Adding the start of a test class for top-level static maps generation.

---
+++ tests/test_static_maps.py
@@ -0,0 +1,33 @@
+import os
+
+from invest_natcap.testing import GISTest
+from invest_natcap import raster_utils
+from adept import static_maps
+
+DATA = os.path.join(os.path.dirname(__file__), '..', 'data')
+CLIPPED_DATA = os.path.join(DATA, 'colombia_clipped')
+FULL_DATA = os.path.join(DATA, 'colombia_tool_data')
+
+class StaticMapTest(GISTest):
+    def test_execute_sediment_smoke(self):
+        config = {
+            "workspace_dir": "",
+            "dem_uri": os.path.join(CLIPPED_DATA, 'dem.tif'),
+            "erosivity_uri": os.path.join(CLIPPED_DATA, "erosivity.tif"),
+            "erodibility_uri": os.path.join(CLIPPED_DATA, "erodibility.tif"),
+            "landuse_uri": "",
+            "watersheds_uri": os.path.join(CLIPPED_DATA, "servicesheds_col.shp"),
+            "reservoir_locations_uri": os.path.join(CLIPPED_DATA, "reservoirs.shp"),
+            "reservoir_properties_uri": "",
+            "biophysical_table_uri": os.path.join(FULL_DATA, "Biophysical_Colombia.csv"),
+            "threshold_flow_accumulation": 4000,
+            "slope_threshold": "5",
+            "sediment_threshold_table_uri": os.path.join(FULL_DATA, "sediment_threshold.csv"),
+        }
+
+        lulc_uri = os.path.join(CLIPPED_DATA, 'ecosystems.tif')
+        workspace = raster_utils.temporary_folder()
+        static_maps.execute_model('sediment', lulc_uri, workspace,
+            config=config)
+
+
