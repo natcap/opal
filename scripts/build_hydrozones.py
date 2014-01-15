7669d20c517616de7bd37b663da58ac4283a847b
James Douglass <jdouglass@stanford.edu>
2014-01-15 15:42:22 -0800

    Update issue 2580
    Locating individual subzones.

--- scripts/build_hydrozones.py
+++ scripts/build_hydrozones.py
@@ -1,5 +1,14 @@
+import os
+import shutil
+
+from invest_natcap import raster_utils
 from osgeo import ogr
 import shapely
+import shapely.geometry
+import shapely.wkb
+import shapely.speedups
+import shapely.prepared
+import shapely.ops
 
 def group_by_attribute(in_vector_uri, out_vector_uri):
     # create a new vector at out_vector_uri
@@ -18,16 +27,48 @@ def group_by_attribute(in_vector_uri, out_vector_uri):
     out_layer = out_vector.CreateLayer(in_defn.GetName(),
         in_layer.GetSpatialRef(), in_defn.GetGeomType())
 
-    # Create the ZH, NOMZH columns
-    for field_name in ['ZH', 'NOMZ']:
+    # Create the ZH, NOMZH columns in the output shapefile.
+    for field_name in ['ZH', 'NOMZH']:
         source_field_index = in_defn.GetFieldIndex(field_name)
-        source_field = in_defn.GetField(source_field_index)
-        new_field = ogr.FieldDefn(source_field_index.GetName(),
-            source_field_index.GetType())
+        source_field = in_defn.GetFieldDefn(source_field_index)
+        new_field = ogr.FieldDefn(source_field.GetName(),
+            source_field.GetType())
         new_field.SetWidth(source_field.GetWidth())
         new_field.SetPrecision(source_field.GetPrecision())
         out_layer.CreateField(new_field)
 
+    # loop through all features and determine which hydrozone they belong to.
+    hydrozones = {}
+    for feature_index in xrange(in_layer.GetFeatureCount()):
+        # get the ZH value
+        feature = in_layer.GetFeature(feature_index)
+        zh_value = feature.GetField('ZH')
+
+        # if the ZH value is not in hydrozones, add the key and create a new
+        # list of geometries.
+        # if the ZH value _is_ in hydrozones, append the feature to the correct
+        # list of geometries.
+        geometry = feature.GetGeometryRef()
+        polygon = shapely.wkb.loads(geometry.ExportToWkb())
+        prepared_polygon = shapely.prepared.prep(polygon)
+        try:
+            hydrozones[zh_value].append(prepared_polygon)
+        except KeyError:
+            hydrozones[zh_value] = [prepared_polygon]
+
+    for key, value in hydrozones.iteritems():
+        print key, len(value)
 
 if __name__ == '__main__':
-    print 'done'
+    workspace = os.path.join(os.getcwd(), 'grouped_hydrozones')
+
+    if os.path.exists(workspace):
+        shutil.rmtree(workspace)
+
+    raster_utils.create_directories([workspace])
+
+    in_vector = os.path.join(os.getcwd(), 'data', 'colombia_tool_data',
+        'Hydrographic_subzones.shp')
+    out_vector = os.path.join(workspace, 'hydrozones.shp')
+
+    group_by_attribute(in_vector, out_vector)
