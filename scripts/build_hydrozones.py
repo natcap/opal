b28531c48a6864dc0346cccfb9d51f08232c1808
James Douglass <jdouglass@stanford.edu>
2014-01-15 16:39:24 -0800

    Update issue 2580
    Finishing the script by adding the new (unioned) geometry to the new
    layer.

--- scripts/build_hydrozones.py
+++ scripts/build_hydrozones.py
@@ -39,10 +39,12 @@ def group_by_attribute(in_vector_uri, out_vector_uri):
 
     # loop through all features and determine which hydrozone they belong to.
     hydrozones = {}
+    hydrozone_names = {}
     for feature_index in xrange(in_layer.GetFeatureCount()):
         # get the ZH value
         feature = in_layer.GetFeature(feature_index)
         zh_value = feature.GetField('ZH')
+        nomzh_value = feature.GetField('NOMZH')
 
         # if the ZH value is not in hydrozones, add the key and create a new
         # list of geometries.
@@ -50,14 +52,41 @@ def group_by_attribute(in_vector_uri, out_vector_uri):
         # list of geometries.
         geometry = feature.GetGeometryRef()
         polygon = shapely.wkb.loads(geometry.ExportToWkb())
-        prepared_polygon = shapely.prepared.prep(polygon)
         try:
-            hydrozones[zh_value].append(prepared_polygon)
+            hydrozones[zh_value].append(polygon)
         except KeyError:
-            hydrozones[zh_value] = [prepared_polygon]
+            hydrozones[zh_value] = [polygon]
+            hydrozone_names[zh_value] = nomzh_value
 
-    for key, value in hydrozones.iteritems():
-        print key, len(value)
+    if shapely.speedups.available:
+        shapely.speedups.enable()
+
+    out_defn = out_layer.GetLayerDefn()
+    for key, contained_subzones in hydrozones.iteritems():
+        print key, len(contained_subzones)
+
+        # get the union
+        union_polygon = shapely.ops.cascaded_union(contained_subzones)
+
+        # make a new OGR feature for the union polygon.
+        feature = ogr.Feature(out_defn)
+        feature.SetField('ZH', key)
+        feature.SetField('NOMZH', hydrozone_names[key])
+
+        # write the new polygon to the new shapefile.
+        geom = ogr.CreateGeometryFromWkb(union_polygon.wkb)
+        feature.SetGeometry(geom)
+
+        out_layer.CreateFeature(feature)
+        feature = None
+        geom = None
+        union_polygon = None
+
+    in_layer = None
+    in_vector = None
+    out_layer = None
+    out_vector = None
+
 
 if __name__ == '__main__':
     workspace = os.path.join(os.getcwd(), 'grouped_hydrozones')
