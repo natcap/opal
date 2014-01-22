9d2fd0fe182608443c0e0499451d4529643af756
James Douglass <jdouglass@stanford.edu>
2014-01-21 17:00:14 -0800

    Update issue 2581
    Adding some (commented out) lines to aggregate raster stats under the
    new ecosystems polygon.  Takes about 5 minutes to run through when
    uncommented.

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,7 +1,9 @@
+import json
 import os
 import shutil
 
 from invest_natcap.testing import GISTest
+from invest_natcap import raster_utils
 
 from adept import preprocessing
 
@@ -20,3 +22,20 @@ class PreprocessingTest(GISTest):
 
         preprocessing.split_multipolygons(ecosystems, out_vector, ['Ecos_dis',
             'Ecosistema', 'FACTOR_DE'])
+
+#        raster_uri = os.path.join(DATA, 'ecosystems.tif')
+#        raster_stats = raster_utils.aggregate_raster_values_uri(raster_uri, out_vector,
+#            shapefile_field='FID')
+#
+#        json_file = open(os.path.join(workspace, 'raster_stats.json'), 'w')
+#        raster_stats_dict = {
+#            'total': raster_stats.total,
+#            'pixel_mean': raster_stats.pixel_mean,
+#            'hectare_mean': raster_stats.hectare_mean,
+#            'n_pixels': raster_stats.n_pixels,
+#            'pixel_min': raster_stats.pixel_min,
+#            'pixel_max': raster_stats.pixel_max,
+#        }
+#        json.dump(raster_stats_dict, json_file, indent=4)
+#        json_file.close()
+
