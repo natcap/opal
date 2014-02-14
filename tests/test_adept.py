b4861132c18ff28be2e754f56d5aafd7ecc81ead
James Douglass <jdouglass@stanford.edu>
2014-02-13 16:47:36 -0800

    Update issue 2623
    Adding a test for the calculation of percent overlap with
    municipalities.

--- tests/test_adept.py
+++ tests/test_adept.py
@@ -5,8 +5,10 @@ import shutil
 from invest_natcap.testing import GISTest
 
 from adept import adept_core
+from adept import analysis
 
 DATA = os.path.join(os.getcwd(), '..', 'data', 'colombia_tool_data')
+TEST_DATA = os.path.join(os.getcwd(), '..', 'data', 'colombia_testing')
 
 class AdeptTest(GISTest):
     def setUp(self):
@@ -43,3 +45,11 @@ class AdeptTest(GISTest):
         adept_core.write_vector(impact_sites, feature_indices, out_vector,
             'all')
 
+    def test_percent_overlap(self):
+        os.makedirs(self.workspace)
+
+        offset_sites = os.path.join(TEST_DATA, 'selected_offsets.shp')
+        municipalities = os.path.join(DATA, 'Municipalities.shp')
+        temp_file = os.path.join(self.workspace, 'tmp_inters_muni.shp')
+
+        analysis.percent_overlap(offset_sites, municipalities, temp_file)
