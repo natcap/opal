e84fb23b21b979f26a9c1df20a4b7e0143851c3d
James Douglass <jdouglass@stanford.edu>
2014-03-06 14:02:03 -0800

    Update issue 2642
    Updating the output static map path and the arguments to the static map
    building function.

--- scripts/build_sediment_sm.py
+++ scripts/build_sediment_sm.py
@@ -9,7 +9,8 @@ FULL_DATA = os.path.join(DATA, 'colombia_tool_data')
 if __name__ == '__main__':
     lulc_uri = os.path.join(FULL_DATA, 'ecosystems.tif')
     target_lucode = 124
-    static_map_uri = 'sediment_static_map_full.tif'
+    workspace = os.path.join(os.getcwd(), 'sediment_static_map')
+    static_map_uri = os.path.join(workspace, 'sediment_static_map_full.tif')
 
     static_maps.build_static_map('sediment', lulc_uri, target_lucode,
-        static_map_uri)
+        static_map_uri, workspace=workspace)
