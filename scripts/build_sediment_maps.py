32041330212d9edcd7ea1d8069f395ce3113fc7a
James Douglass <jdouglass@stanford.edu>
2014-03-10 09:57:48 -0700

    Update issue 2680
    Using the actual lulc codes for the sediment maps building script.

--- scripts/build_sediment_maps.py
+++ scripts/build_sediment_maps.py
@@ -8,10 +8,11 @@ FULL_DATA = os.path.join(DATA, 'colombia_tool_data')
 
 if __name__ == '__main__':
     args = {
-        'paved_landcover_code': 100,
-        'bare_landcover_code': 124,
+        'paved_landcover_code': 89,
+        'bare_landcover_code': 301,
         'workspace_dir': os.path.join(os.getcwd(), 'sediment_static_maps'),
         'model_name': 'sediment',
         'landuse_uri': os.path.join(FULL_DATA, 'ecosystems.tif'),
+        'use_parallelism': True,
     }
     static_maps.execute(args)
