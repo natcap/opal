932759f8e36bba6369e33ad730bf8c9d6ebe492e
James Douglass <jdouglass@stanford.edu>
2014-03-15 21:24:26 -0700

    Update issue 2694
    Adding a static map generation script for nutrient model.

---
+++ scripts/build_nutrient_maps.py
@@ -0,0 +1,18 @@
+import os
+
+from adept import static_maps
+
+DATA = os.path.join(os.path.dirname(__file__), '..', 'data')
+CLIPPED_DATA = os.path.join(DATA, 'colombia_clipped')
+FULL_DATA = os.path.join(DATA, 'colombia_tool_data')
+
+if __name__ == '__main__':
+    args = {
+        'paved_landcover_code': 89,
+        'bare_landcover_code': 301,
+        'workspace_dir': os.path.join(os.getcwd(), 'nutrient_static_maps'),
+        'model_name': 'nutrient',
+        'landuse_uri': os.path.join(FULL_DATA, 'ecosystems.tif'),
+        'do_parallelism': True,
+    }
+    static_maps.execute(args)
