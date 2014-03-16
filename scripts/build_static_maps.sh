904094efd66a4eba5c87b5ed17626ff1c4620c12
James Douglass <jdouglass@stanford.edu>
2014-03-15 22:07:08 -0700

    Update issue 2694
    Adding a shell script to build all the static maps.

---
+++ scripts/build_static_maps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# EXECUTE THIS FROM THE PERMITTING ROOT
+
+ENVDIR=adept_py_env
+deactivate
+
+rm -rf build
+rm -rf $ENVDIR
+python bootstrap_adept_environment.py > setup_environment.py
+python setup_environment.py --clear --system-site-packages $ENVDIR
+source $ENVDIR/bin/activate
+
+pushd invest-natcap.invest-3
+# IF YOU WOULD LIKE TO REBUILD ALL OF INVEST-3, UNCOMMENT THIS NEXT LINE
+#hg purge  # clean out the invest-3 repo
+python setup.py install
+
+popd
+
+# return to adept dir to build adept.
+pushd adept
+python setup.py install
+popd
+
+python scripts/build_carbon_maps.py &
+python scripts/build_sediment_maps.py &
+python scripts/build_nutrient_maps.py &
