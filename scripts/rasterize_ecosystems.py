b6cf5bf9513c643d65d19457b3394f9f20612494
James Douglass <jdouglass@stanford.edu>
2014-03-20 09:40:02 -0700

    Update issue 2612
    Fixing a critical issue with the generation of ecosystems.tif from the
    ecosystems vector.  The old version of this script used a byte raster,
    despite the fact that there were more values to capture than just 0-256.
    The current script now uses an Int16 raster.

--- scripts/rasterize_ecosystems.py
+++ scripts/rasterize_ecosystems.py
@@ -1,11 +1,11 @@
 import gdal
 from invest_natcap import raster_utils
 
-lulc_vector = 'data/colombia_clipped/ecosystems_colombia.shp'
-target_raster = 'data/colombia_clipped/ecosystems.tif'
-sample_raster = 'data/colombia_clipped/dem.tif'
+lulc_vector = 'data/colombia_tool_data/Ecosystems_Colombia.shp'
+target_raster = 'data/colombia_tool_data/ecosystems.tif'
+sample_raster = 'data/colombia_tool_data/DEM.tif'
 
-raster_utils.new_raster_from_base_uri(sample_raster, target_raster, 'GTiff', -1,
+raster_utils.new_raster_from_base_uri(sample_raster, target_raster, 'GTiff', 0,
         gdal.GDT_Int16)
 raster_utils.rasterize_layer_uri(target_raster, lulc_vector,
         option_list=["ATTRIBUTE=lucode"])
