b5657abf2c3cd7a0071e3185fc9a708489c76542
James Douglass <jdouglass@stanford.edu>
2014-03-20 09:41:07 -0700

    Update issue 2740
    Adding the future landcover argument to the carbon static map generation
    script.

--- scripts/build_carbon_maps.py
+++ scripts/build_carbon_maps.py
@@ -13,5 +13,6 @@ if __name__ == '__main__':
         'workspace_dir': os.path.join(os.getcwd(), 'carbon_static_maps'),
         'model_name': 'carbon',
         'landuse_uri': os.path.join(FULL_DATA, 'ecosystems.tif'),
+        'fut_landuse_uri': os.path.join(FULL_DATA, 'es_comp_rd.tif'),
     }
     static_maps.execute(args)
