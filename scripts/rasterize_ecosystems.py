5fb9d45d26250bcde85c7c93a06496b3aacd9019
James Douglass <jdouglass@stanford.edu>
2014-03-20 09:11:04 -0700

    Fixing a critical bug in the rasterization of the ecosystems raster ... I was saving a byte raster, not an Int16 raster.  This is problematic because lucodes go up to 315, which is outside of the range permitted by 2^8.

--- scripts/rasterize_ecosystems.py
+++ scripts/rasterize_ecosystems.py
@@ -6,6 +6,6 @@ target_raster = 'data/colombia_clipped/ecosystems.tif'
 sample_raster = 'data/colombia_clipped/dem.tif'
 
 raster_utils.new_raster_from_base_uri(sample_raster, target_raster, 'GTiff', -1,
-        gdal.GDT_Byte)
+        gdal.GDT_Int16)
 raster_utils.rasterize_layer_uri(target_raster, lulc_vector,
         option_list=["ATTRIBUTE=lucode"])
