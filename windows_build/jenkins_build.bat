c12948ae6ca1366aea4daa7e526c1f2dc33d3bac
James Douglass <jdouglass@stanford.edu>
2014-04-04 13:43:46 -0700

    Update issue 2722
    Requiring pyinstaller to use a console.

--- windows_build/jenkins_build.bat
+++ windows_build/jenkins_build.bat
@@ -31,5 +31,5 @@ cd ..
 
 :: Now that everything is installed, we can run the permitting project's
 :: setup.py commands to build everything we want/need.
-%ENVDIR%\Scripts\python src\pyinstaller\pyinstaller.py -y --onedir --noupx run_adept.spec
+%ENVDIR%\Scripts\python src\pyinstaller\pyinstaller.py -y --onedir --noupx -c run_adept.spec
 %ENVDIR%\Scripts\python setup.py zip_data win_installer --nsis-dir=dist/run_adept_coll
