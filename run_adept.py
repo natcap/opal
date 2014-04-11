a90cd4d7436198ee8cd61a6886ff432cfbed96a6
James Douglass <jdouglass@stanford.edu>
2014-04-11 10:27:26 -0700

    Update issue 2786
    Greatly simplifying the adept launch script.

--- run_adept.py
+++ run_adept.py
@@ -1,14 +1,3 @@
 import palisades
-from palisades import elements
-import adept
-import os
-import sys
 
-if adept.is_frozen():
-    adept_uri = os.path.join(adept.get_frozen_dir(), 'adept.json')
-    #adept_uri = os.path.join(sys._MEIPASS, 'adept.json')
-else:
-    adept_uri = 'adept.json'
-ui = elements.Application(adept_uri)
-gui = palisades.gui.build(ui._window)
-gui.execute()
+palisades.launch('adept.json')
