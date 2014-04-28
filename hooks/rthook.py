71782f2c3a2bd325fbce4c9b5c32178ae540a4d3
James Douglass <jdouglass@stanford.edu>
2014-04-28 11:42:21 -0700

    Update issue 2674
    Adding a call to multiprocessing.freeze_support in the runtime hook.

--- hooks/rthook.py
+++ hooks/rthook.py
@@ -1,6 +1,7 @@
 import sys
 import os
-import adept
-from adept import static_maps
+import multiprocessing
+
+multiprocessing.freeze_support()
 
 os.environ['MATPLOTLIBDATA'] = os.path.join(sys._MEIPASS, 'mpl-data')
