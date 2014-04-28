71baaa379c04c16ddf43c4072d008586b6215993
James Douglass <jdouglass@stanford.edu>
2014-04-28 13:38:38 -0700

    Update issue 2674
    Re-adding the adept imports to the runtime hook because I'm currently
    using them to guarantee that we import adept and adept.static_maps,
    which will allow palisades to find the modules later on.

--- hooks/rthook.py
+++ hooks/rthook.py
@@ -2,6 +2,12 @@ import sys
 import os
 import multiprocessing
 
+# import adept and static_maps here so that we absolutely guarantee that we're
+# going to have access to these packages when palisades needs to do its dynamic
+# imports.
+import adept
+from adept import static_maps
+
 multiprocessing.freeze_support()
 
 os.environ['MATPLOTLIBDATA'] = os.path.join(sys._MEIPASS, 'mpl-data')
