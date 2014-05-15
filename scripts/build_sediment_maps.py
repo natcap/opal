b3c34249df9ecaa1890df32e1edec3294bf4fc68
James Douglass <jdouglass@stanford.edu>
2014-05-15 15:05:39 -0700

    Update issue 2941
    Adding an optional --test argument to the sediment static map builder,
    which, when used, uses some downsampled data in the SVN repo.

--- scripts/build_sediment_maps.py
+++ scripts/build_sediment_maps.py
@@ -1,12 +1,14 @@
 import os
 import logging
 import shutil
+import sys
 
 from adept import static_maps
 
 DATA = os.path.join(os.path.dirname(__file__), '..', 'data')
 CLIPPED_DATA = os.path.join(DATA, 'colombia_clipped')
 FULL_DATA = os.path.join(DATA, 'colombia_tool_data')
+TEST_DATA = os.path.join(DATA, 'colombia_testing')
 
 LOGGER = logging.getLogger('sediment_sm_cli')
 
@@ -20,6 +22,18 @@ if __name__ == '__main__':
         'fut_landuse_uri': os.path.join(FULL_DATA, 'es_comp_rd.tif'),
         'do_parallelism': True,
     }
+
+    try:
+        argument_1 = sys.argv[1]
+    except IndexError:
+        argument_1 = False
+
+    if argument_1 == '--test':
+        LOGGER.debug('Using testing URIs instead')
+        args['landuse_uri'] = os.path.join(TEST_DATA, 'ecosystems_small_lzw.tif')
+        args['fut_landuse_uri'] = os.path.join(TEST_DATA, 'es_comp_rd_small_lzw.tif')
+        args['dem_uri'] = os.path.join(TEST_DATA, 'DEM_small_lzw.tif')
+
     if os.path.exists(args['workspace_dir']):
         shutil.rmtree(args['workspace_dir'])
     os.makedirs(args['workspace_dir'])
