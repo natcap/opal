d67342a7ae8f505254cf27036f935ee4ff59436c
James Douglass <jdouglass@stanford.edu>
2014-06-24 10:31:56 -0700

    Updating the extract_impact_stats scripts to run multiple aggregations as multiprocessing processes.

--- scripts/extract_impact_stats.py
+++ scripts/extract_impact_stats.py
@@ -3,35 +3,33 @@
 
 import glob
 import os
+import multiprocessing
 
 from adept import static_maps
 from invest_natcap import raster_utils
 
-if __name__ == '__main__':
-    #watershed_base_dir = 'F:/sediment_map_quality_backup/Bare'
-    watershed_base_dir = '/home/jadoug06/workspace/invest-natcap.permitting/ignore_me/sediment_map_quality/bare'
+def extract_impact_data(service, scenario_dir):
     base_watersheds = os.path.join(os.getcwd(), 'data', 'colombia_tool_data',
         'watersheds_cuencas.shp')
 
-    model_name = 'sediment'
+    model_name = service
     base_static_map = os.path.join(os.getcwd(), 'data', 'colombia_static_data',
             '%s_%s_static_map_lzw.tif' % (model_name,
-            watershed_base_dir.split('/')[-1].lower()))
+            scenario_dir.split('/')[-1].lower()))
 
     base_stats = raster_utils.aggregate_raster_values_uri(base_static_map,
         base_watersheds, 'ws_id', 'sum').total
 
-    logfile_uri = os.path.join(watershed_base_dir, 'scraped_simulations.csv')
+    logfile_uri = os.path.join(scenario_dir, 'scraped_simulations.csv')
     logfile = open(logfile_uri, 'w')
     labels = ['ws_id', 'Impact ID', 'Impact area', 'Static estimate',
         'InVEST estimate', 'Estimate ratio']
     logfile.write('%s\n' % ','.join(labels))
 
-    for watershed_id in range(40, 43):
-        watershed_dir = os.path.join(watershed_base_dir, 'watershed_%s' %
+    for watershed_id in range(54):
+        watershed_dir = os.path.join(scenario_dir, 'watershed_%s' %
             watershed_id)
-        print watershed_dir
-        watershed_vector = os.path.join(watershed_base_dir, 'watershed_vectors',
+        watershed_vector = os.path.join(scenario_dir, 'watershed_vectors',
             'feature_%s.shp' % watershed_id)
 
         glob_pattern = os.path.join(watershed_dir, 'random_impact_*')
@@ -48,4 +46,23 @@ if __name__ == '__main__':
                 impact_stats['export_ratio'],
             ]
             logfile.write('%s\n' % ','.join(map(str, stats_to_write)))
+
+if __name__ == '__main__':
+    #watershed_base_dir = 'F:/sediment_map_quality_backup/Bare'
+    #watershed_base_dir = '/home/jadoug06/workspace/invest-natcap.permitting/ignore_me/sediment_map_quality/bare'
+    processes = []
+    watershed_base_dir = '/colossus'
+    for service in ['nutrient', 'sediment']:
+        service_dir = os.path.join(watershed_base_dir, '%s_simulations' % service)
+        for scenario in ['bare', 'paved']:
+            scenario_dir = os.path.join(service_dir, scenario)
+            print scenario_dir
+            p = multiprocessing.Process(target=extract_impact_data,
+                args=(service, scenario_dir))
+            p.start()
+            processes.append(p)
+
+
+    for p in processes:
+        p.join()
 
