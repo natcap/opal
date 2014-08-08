3024a4d4c6852a9338ea4057e610f0a972bb1979
James Douglass <jdouglass@stanford.edu>
2014-08-08 14:27:29 -0700

    Adding splash screen to main MAFE/OPAL interface.

--- run_adept.py
+++ run_adept.py
@@ -1,7 +1,15 @@
 import palisades
+import sys
+import os
 
 print "Build data"
 for attr in palisades.build_data:
     print "%s: %s" % (attr, getattr(palisades, attr, False))
 
-palisades.launch('adept.json')
+if getattr(sys, 'frozen', False):
+    # If we're in a pyinstaller build
+    splash = os.path.join(os.path.dirname(sys.executable), 'splash.jpg')
+else:
+    splash = os.path.join('windows_build', 'OPAL.jpg')
+
+palisades.launch('adept.json', splash)
