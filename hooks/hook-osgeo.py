78cfe4edba5925b43802a9740a724c82497b865d
James Douglass <jdouglass@stanford.edu>
2014-10-09 11:55:56 -0700

    Update issue 3140
    Adding a printout of the exception encountered when we default to using
    osgeo for geos_c.dll

--- hooks/hook-osgeo.py
+++ hooks/hook-osgeo.py
@@ -10,8 +10,9 @@ if is_win:
     try:
         import shapely
         pkg_base, pkg_dir = get_package_paths('shapely')
-    except ImportError, AssertionError:
+    except ImportError, AssertionError as error:
         # ImportError is raised when we can't import shapely
         # AssertionError is raised when the package path can't be found.
+        print 'Defaulting to osgeo pkg:', error
         pkg_base, pkg_dir = get_package_paths('osgeo')
     datas = [(os.path.join(pkg_dir, filename), '') for filename in files]
