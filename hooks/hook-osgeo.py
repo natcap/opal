45e8af5ee5ad799d1b7c8de900e776e42a6bb889
James Douglass <jdouglass@stanford.edu>
2014-10-09 10:23:09 -0700

    Update issue 3140
    Adding a check in hook-osgeo for whether shapely is present.  If it is,
    we should prefer to use that version of GEOS_C.dll, as it seems to fix
    the issue 3143.

--- hooks/hook-osgeo.py
+++ hooks/hook-osgeo.py
@@ -6,5 +6,10 @@ if is_win:
     files = [
         'geos_c.dll',
     ]
-    pkg_base, pkg_dir = get_package_paths('osgeo')
+    # If shapely is present, use that version of GEOS_C.dll
+    try:
+        import shapely
+        pkg_base, pkg_dir = get_package_paths('Shapely')
+    except ImportError:
+        pkg_base, pkg_dir = get_package_paths('osgeo')
     datas = [(os.path.join(pkg_dir, filename), '') for filename in files]
