390e33cf9dcd1229ef7ffe0a118d1cdd464eb40d
James Douglass <jdouglass@stanford.edu>
2014-10-21 10:01:07 -0700

    Update issue 3169
    Adding argument parsing to the run_opal UI launch script.

--- run_opal.py
+++ run_opal.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 
 import palisades
 import palisades.i18n
@@ -16,15 +17,22 @@ class MultilingualRunner(execution.PythonRunner):
         adept.i18n.language.set(palisades_lang)
         execution.PythonRunner.start(self)
 
-print "Build data"
-for attr in palisades.build_data:
-    print "%s: %s" % (attr, getattr(palisades, attr, False))
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser(
+        description='Fire up the OPAL (or derivative) UI.')
+    args_parser.add_argument('json_config',  metavar='json_config',
+        help='JSON configuration file')
+    args = args_parser.parse_args()
 
-if getattr(sys, 'frozen', False):
-    # If we're in a pyinstaller build
-    splash = os.path.join(os.path.dirname(sys.executable), 'splash.png')
-else:
-    splash = os.path.join(os.getcwd(), 'windows_build', 'OPAL.png')
-print 'splash image: %s' % splash
+    print "Build data"
+    for attr in palisades.build_data:
+        print "%s: %s" % (attr, getattr(palisades, attr, False))
 
-palisades.launch('adept.json', splash, MultilingualRunner)
+    if getattr(sys, 'frozen', False):
+        # If we're in a pyinstaller build
+        splash = os.path.join(os.path.dirname(sys.executable), 'splash.png')
+    else:
+        splash = os.path.join(os.getcwd(), 'windows_build', 'OPAL.png')
+    print 'splash image: %s' % splash
+
+    palisades.launch(args.json_config, splash, MultilingualRunner)
