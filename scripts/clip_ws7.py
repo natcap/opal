5fb368cb0bbc824d3c9d305f47d0b9481ddb288e
James Douglass <jdouglass@stanford.edu>
2014-11-11 09:28:56 -0800

    Updating the ws-7 clip script.

--- scripts/clip_ws7.py
+++ scripts/clip_ws7.py
@@ -4,9 +4,11 @@ from adept import static_maps
 if __name__ == '__main__':
     ws_vector = '/colossus/colombia_sdr/bare/simulations/watershed_vectors/feature_7.shp'
 
+    raster_name = 'usle'
+
     for scenario in ['base', 'bare']:
-        big_raster = '/home/jadoug06/ws7/%s_sdr_factor.tif' % scenario
-        small_raster = '/home/jadoug06/ws7/ws7_%s_sdr_factor.tif' % scenario
+        big_raster = '/home/jadoug06/ws7/%s_%s.tif' % (scenario, raster_name)
+        small_raster = '/home/jadoug06/ws7/ws7_%s_%s.tif' % (scenario, raster_name)
 
         print 'clipping ', scenario
         static_maps.clip_static_map(big_raster, ws_vector, small_raster)
