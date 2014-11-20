55b2bf1dadbc1e7204953f75b12f3707fbb39c1d
James Douglass <jdouglass@stanford.edu>
2014-11-20 15:58:27 -0800

    Adding a script to nicely package up the target watershed's impact 0 for a target watershed simulation.

---
+++ scripts/test_sdr/package_sample_ws.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#  Take relevant outputs from the watershed dir at arg1 and zip them up.
+#
+
+TMPDIR=_tmp_ws_outputs
+
+rm -rf $TMPDIR
+mkdir $TMPDIR
+
+
+IMPACT_DIR=$1/random_impact_0
+
+# grab the impact site vector
+cp $IMPACT_DIR/impact_0.* $TMPDIR
+
+# grab the impact USLE, SDR, sed_export rasters
+cp $IMPACT_DIR/intermediate/sdr_factor.tif $TMPDIR/impact_sdr_factor.tif
+cp $IMPACT_DIR/output/sed_export.tif $TMPDIR/impact_sed_export.tif
+cp $IMPACT_DIR/output/usle.tif $TMPDIR/impact_usle.tif
+
+cp $1/watershed_base_sdr.tif $TMPDIR/base_sdr_factor.tif
+cp $1/watershed_base_sed_exp.tif $TMPDIR/base_sed_export.tif
+cp $1/watershed_usle.tif $TMPDIR/base_usle.tif
+
+BASE_DIR=`basename $1`
+IMPACT=`basename $IMPACT_DIR | egrep -o '[0-9]+$'`
+ZIPNAME=$BASE_DIR-impact_$IMPACT.zip
+zip -r $ZIPNAME $TMPDIR
