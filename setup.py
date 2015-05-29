dcf79bd4e7212157efcf437b5973598127997a84
James Douglass <jdouglass@stanford.edu>
2015-05-28 18:22:47 -0700

    Update issue 3362
    If palisades' HEAD is in a detatched state, that's probably ok, so no
    need to raise an exception.  The HEAD is pointing to a previous version
    of the source code, and it's ok if the versioning logic isn't quite
    working as expected.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ print 'Palisades imported from %s' % palisades.__file__
 #     $ git checkout master
 # And then re-run the build.
 if palisades.__version__ == 'remotes/origin/HEAD':
-    raise Exception('palisades version is invalid: %s' % palisades.__version__)
+    LOGGER.error('palisades version is invalid: %s' % palisades.__version__)
 
 py2exe_options = {}
 
