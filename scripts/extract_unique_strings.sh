326241cec6c8d68365148d0d784f184a0ce1f178
James Douglass <jdouglass@stanford.edu>
2015-09-15 11:06:38 -0700

    Updating the scripts to extract unique strings from the UIs

--- scripts/extract_unique_strings.sh
+++ scripts/extract_unique_strings.sh
@@ -1,6 +1,12 @@
-for file in {adept,carbon_sm,generic_sm,nutrient_sm,sediment_sm}.json;
+if [ -e 'out_file.txt' ]
+then
+    echo 'out_file.txt exists. Remove and re-run'
+    exit 1
+fi
+
+for file in {opal,opal_carbon_sm,opal_generic_sm,opal_nutrient_sm,opal_sediment_sm}.json;
 do 
-    echo "UI FILE $file"
+    echo "UI FILE $file" >> out_file.txt
     scripts/extract_text.sh $file >> out_file.txt
 done
-python scripts/unique_strings.py out_file.txt
+python scripts/unique_strings.py out_file.txt > opal_unique_strings.txt
