21082854c1102f6681455046f9dfca0d450d04cc
James Douglass <jdouglass@stanford.edu>
2017-08-03 17:03:40 -0700

    Replacing osgeo hook with the osgeo hook from InVEST.
    The reason for this is that replacing all of the OSGEO DLLs in the OPAL
    build with those from InVEST causes the build to run as expected.
    RE:#BITBUCKET-3379

--- hooks/hook-osgeo.py
+++ hooks/hook-osgeo.py
@@ -1,39 +1,12 @@
-from PyInstaller.compat import is_win
-from PyInstaller.hooks.hookutils import get_package_paths
-from osgeo import gdal
-import platform
+from PyInstaller.compat import is_darwin
 import os
-import glob
 
-files = []
-if is_win:
-    files += [
-        'geos_c.dll',
-    ]
-    # If shapely is present, use that version of GEOS_C.dll
-    try:
-        import shapely
-        pkg_dir = os.path.dirname(shapely.__file__)
-    except (ImportError, AssertionError) as error:
-        # ImportError is raised when we can't import shapely
-        # AssertionError is raised when the package path can't be found.
-        print 'Defaulting to osgeo pkg:', error
-        pkg_base, pkg_dir = get_package_paths('osgeo')
+if is_darwin:
+    # Assume we're using homebrew to install GDAL and collect data files
+    # accordingly.
+    from PyInstaller.utils.hooks import get_homebrew_path
+    from PyInstaller.utils.hooks import collect_system_data_files
 
-    osgeo_base, osgeo_dir = get_package_paths('osgeo')
-    data_dir = os.path.join(osgeo_dir, 'data', 'gdal')
-else:
-    if platform.system() == 'Darwin':
-        # Accommodate multiple gdal version installations
-        _base_data_dir = '/usr/local/Cellar/gdal/'
-        _version_glob = "%s*" % gdal.__version__
-        _gdal_version_dirname = glob.glob(_base_data_dir + _version_glob)[-1]
-        data_dir = os.path.join(_gdal_version_dirname, 'share', 'gdal')
-    else:
-        # assume platform.system() == 'Linux'
-        raise Exception('Not yet implemented')
-
-datas = [(os.path.join(pkg_dir, filename), '') for filename in files]
-
-datas += [(os.path.join(data_dir, filename), '') for filename in
-        glob.glob(data_dir + '/*')]
+    datas = collect_system_data_files(
+        path=os.path.join(get_homebrew_path('gdal'), 'share', 'gdal'),
+        destdir='gdal-data')
