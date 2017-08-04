1c3efc582fe8578b71a468dcf7505ad5ace88e36
James Douglass <jdouglass@stanford.edu>
2017-08-04 11:35:43 -0700

    Updating pyinstaller (and hooks) to v3.2.1 to try to match InVEST build.

--- hooks/hook-natcap.py
+++ hooks/hook-natcap.py
@@ -1,3 +1,4 @@
-from PyInstaller.hooks.hookutils import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+
 datas = collect_data_files('natcap')
 hiddenimports = collect_submodules('natcap')
